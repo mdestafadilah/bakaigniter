ca6dde9c76856d46ecb3b3afeec978c50a333ebc
Fery Wardiyanto <ferywardiyanto@gmail.com>
2014-06-18 00:35:24 +0700

    update config/bakaigniter.php file

--- config/bakaigniter.php
+++ config/bakaigniter.php
@@ -12,67 +12,67 @@
 | Default application configurations
 |--------------------------------------------------------------------------
 */
-// BAKA Application Name
+// Baka Igniter Application Name
 $config['baka_app_name']            = 'BPMPPT App';
-// BAKA Application Description
+// Baka Igniter Application Description
 $config['baka_app_desc']            = 'Aplikasi pengelolaan data perijinan di BPMPPT Kab. Pekalongan';
-// BAKA Application Description
+// Baka Igniter Application Description
 $config['baka_app_client']          = 'BPMPPT Kab. Pekalongan';
-// BAKA Application Version
+// Baka Igniter Application Version
 $config['baka_app_version']         = '0.1.4';
-// BAKA Application Author
+// Baka Igniter Application Author
 $config['baka_app_author']          = 'Fery Wardiyanto';
-// BAKA Application Author Email
+// Baka Igniter Application Author Email
 $config['baka_app_author_email']    = 'ferywardiyanto@gmail.com';
-// BAKA Application Author Url
+// Baka Igniter Application Author Url
 $config['baka_app_author_url']      = 'http://feryardiant.github.io';
-// BAKA Application Repository Url
+// Baka Igniter Application Repository Url
 $config['baka_app_repo_url']        = 'http://github.com/feryardiant/bpmppt';
-// BAKA Application Documentation
+// Baka Igniter Application Documentation
 $config['baka_app_doc_url']         = '';
 
-// BAKA Minimal Web Browser Version
+// Baka Igniter Minimal Web Browser Version
 $config['baka_app_min_browser']     = array(
     'Firefox'           => 4,
     'Chrome'            => 8,
     'Internet Explorer' => 8,
     );
 
-// BAKA Igniter Base Path
-$config['baka_base_path']     = dirname(dirname(__FILE__)).'/';
+// Baka Igniter Base Path
+$config['baka_base_path']           = dirname(dirname(__FILE__)).'/';
 
 /*
 |--------------------------------------------------------------------------
 | Table name configurations
 |--------------------------------------------------------------------------
 */
-// BAKA media table name
+// Baka Igniter media table name
 $config['baka_media_table']             = 'media';
 
-// BAKA system_opt table name
+// Baka Igniter system_opt table name
 $config['baka_system_opt_table']        = 'system_opt';
-// BAKA system_env table name
+// Baka Igniter system_env table name
 $config['baka_system_env_table']        = 'system_env';
 
-// BAKA users table name
+// Baka Igniter users table name
 $config['baka_users_table']             = 'auth_users';
-// BAKA user_meta table name
+// Baka Igniter user_meta table name
 $config['baka_user_meta_table']         = 'auth_usermeta';
-// BAKA user_profiles table name
+// Baka Igniter user_profiles table name
 $config['baka_user_profile_table']      = 'auth_user_profiles';
-// BAKA user_group table name
+// Baka Igniter user_group table name
 $config['baka_user_role_table']         = 'auth_user_roles';
-// BAKA user_meta table name
+// Baka Igniter user_meta table name
 $config['baka_roles_table']             = 'auth_roles';
-// BAKA user_permission table name
+// Baka Igniter user_permission table name
 $config['baka_permissions_table']       = 'auth_permissions';
-// BAKA user_permission table name
+// Baka Igniter user_permission table name
 $config['baka_role_perms_table']        = 'auth_role_permissions';
-// BAKA user_permission table name
+// Baka Igniter user_permission table name
 $config['baka_overrides_table']         = 'auth_overrides';
-// BAKA users table name
+// Baka Igniter users table name
 $config['baka_user_autologin_table']    = 'auth_user_autologin';
-// BAKA users table name
+// Baka Igniter users table name
 $config['baka_login_attempts_table']    = 'auth_login_attempts';
 
 /*
@@ -93,11 +93,11 @@ $config['baka_default_meta_fields'] = array(
 | File Upload configuration
 |--------------------------------------------------------------------------
 */
-// BAKA temporary folder name
+// Baka Igniter temporary folder name
 $config['baka_temp_path']           = APPPATH.'storage/tmp/';
-// BAKA upload folder name
+// Baka Igniter upload folder name
 $config['baka_upload_path']         = APPPATH.'storage/upload/';
-// BAKA allowed file types
+// Baka Igniter allowed file types
 $config['baka_allowed_types']       = 'gif|jpg|jpeg|png';
 $config['baka_thumb_height']        = 100;
 $config['baka_thumb_width']         = 100;
