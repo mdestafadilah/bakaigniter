999ea2ca887dae573e4376684bf0c66d6ecaa210
Fery Wardiyanto <ferywardiyanto@gmail.com>
2014-06-28 08:20:03 +0700

    Update configuration.sh script and make things clean

--- configure.sh
+++ configure.sh
@@ -2,8 +2,15 @@
 echo "Downloading CodeIgniter v2.2.0"
 wget https://github.com/EllisLab/CodeIgniter/archive/2.2.0.tar.gz -qO - | tar xz
 
-echo "Preparing"
+echo "Preparing:"
+echo " - Moving files"
 mkdir samples/application/bakaigniter
+for file in index.html license.md README.md; do
+    mv $file samples/application/bakaigniter/$file
+done
+unset file
+
+echo " - Moving folders"
 for dir in config helpers language libraries; do
     mkdir samples/application/bakaigniter/$dir/
     cp -r $dir/* samples/application/bakaigniter/$dir/
@@ -17,4 +24,7 @@ cp -r CodeIgniter-2.2.0/* .
 
 echo "Cleaning"
 rm -rf CodeIgniter-2.2.0
+rm -rf user_guide
 rm -rf samples
+
+echo "Done!"
